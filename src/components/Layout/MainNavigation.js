abb7d6330fade950026db67dfda916d34de67850
Nurullo <khuwaidullaev@gmail.com>
2021-07-29 13:23:23 +0500

    Added Logout functionality

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,11 +1,15 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
 
   return (
     <header className={classes.header}>
@@ -26,7 +30,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
